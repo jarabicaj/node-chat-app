ec46ea0486ed3205e7932b1ca4516a458f91400c
jarabicaj <jarabica.j@gmail.com>
2018-08-03 20:52:00 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -19,14 +19,35 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', { // send to everyone who is connected
-            message: message.from,
+        // send to everyone who is connected INCLUDE ME
+        io.emit('newMessage', {
+            from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // // send to everyone??? who is connected EXCLUDE ME
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', () => {
